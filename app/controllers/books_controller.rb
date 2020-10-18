eb17793144c93c6f84340b035970b2e301cbfffc
EC2 Default User <ec2-user@ip-172-31-42-2.ec2.internal>
2020-10-18 08:57:45 +0000

    [update] 4-9 あとはエラーメッセージとサクセスメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,6 +25,7 @@ class BooksController < ApplicationController
 
   def edit
    @book = Book.find(params[:id])
+   
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'books#top'
   get 'home/about' => 'books#about'
+  
    devise_for :users
   resources :users, only: [:index, :edit, :update, :show]
 
