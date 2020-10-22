21863dd7547a1f57092d6f8140477487b0ae30f4
EC2 Default User <ec2-user@ip-172-31-42-2.ec2.internal>
2020-10-22 06:11:58 +0000

    [finish] 4-9 課題３再提出

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_113843) do
+ActiveRecord::Schema.define(version: 2020_10_13_041734) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "user_id"
+    t.integer "user_id"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,10 +27,10 @@ ActiveRecord::Schema.define(version: 2020_10_15_113843) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.text "introduction"
+    t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "profile_image_id"
-    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
