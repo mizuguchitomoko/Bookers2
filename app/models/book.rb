bd3868f752ad8a3314d278bb708931be1ddb86b3
EC2 Default User <ec2-user@ip-172-31-42-2.ec2.internal>
2020-10-24 02:54:33 +0000

    [finish] 4-9 再提出

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,4 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true,
                    length: { maximum: 200 }
-   has_many :profile_image, dependent: :destroy
-   attachment :profile_image
 end
